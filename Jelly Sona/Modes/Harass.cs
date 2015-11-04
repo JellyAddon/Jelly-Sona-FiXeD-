46b0ffd744ad2ba704bf45849ec3fdd0d6133895
2beautiful9uy <zxc63635@naver.com>
2015-11-04 21:30:41 +0900

    Jelly-Sona-Fixed

--- Jelly Sona/Modes/Harass.cs
+++ Jelly Sona/Modes/Harass.cs
@@ -19,9 +19,9 @@ namespace Jelly.Sona.Modes
             // TODO: Add harass logic here
             // See how I used the Settings.UseQ and Settings.Mana here, this is why I love
             // my way of using the menu in the Config class!
-            if (Settings.UseQ && Player.Instance.ManaPercent > Settings.Mana && Q.IsReady())
+            if (Settings.UseQ && Q.IsReady())
             {
-                var target = TargetSelector.GetTarget(Q.Range, DamageType.Physical);
+                var target = TargetSelector.GetTarget(Q.Range, DamageType.Magical);
                 if (target != null)
                 {
                     Q.Cast();
